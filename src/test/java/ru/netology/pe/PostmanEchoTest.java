a8741136d22242bb17561eab8bdcaee15e9ac1b7
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-09 11:39:31 +0300

    test failed

--- src/test/java/ru/netology/pe/PostmanEchoTest.java
+++ src/test/java/ru/netology/pe/PostmanEchoTest.java
@@ -2,21 +2,22 @@ package ru.netology.pe;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 class PostmanEchoTest {
     @Test
     void shouldReturnRequest() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
+
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-//                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("Hell"))
         ;
     }
 }
